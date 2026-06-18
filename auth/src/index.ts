1ad69ff90ffa10dd7f94e6d3974ac936a85e2d04
Vaibhav Kadwey <vaibhav3kadwey@gmail.com>
2026-06-19 01:31:13 +0530

    added : connection to mongo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signUpRouter } from "./routes/signup";
 import { signInRouter } from "./routes/signin";
@@ -23,6 +25,19 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port: 3000 ");
-});
+const startApp = async () => {
+  /** First we try to connect to database and when we have successfully connected */
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to Mongo server");
+  } catch (err) {
+    console.error(err);
+  }
+
+  /** Start the server */
+  app.listen(3000, () => {
+    console.log("Listening on port: 3000 ");
+  });
+};
+
+startApp();
