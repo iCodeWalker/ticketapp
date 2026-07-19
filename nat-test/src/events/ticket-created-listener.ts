a2ed94303e5cd5b6cda06fc79e44ee5f1d390558
Vaibhav Kadwey <vaibhav3kadwey@gmail.com>
2026-07-19 17:29:37 +0530

    added : type checks for the listeners

--- nat-test/src/events/ticket-created-listener.ts
+++ nat-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,12 @@
 import nats, { Message, Stan } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created"; // Channel name
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: TicketCreatedEvent["subject"] = Subjects.TicketCreated; // "ticket:created"; // Channel name
   queueGroupName = "payments-service";
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("Event data:", data);
 
     msg.ack();
