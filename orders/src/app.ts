378602b0e410a222a741165aa1d7ed9545d2f89b
Vaibhav Kadwey <vaibhav3kadwey@gmail.com>
2026-07-30 00:10:34 +0530

    added : code for creating a new orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import {
+  NotFoundError,
+  errorHandler,
+  currentUser,
+} from "@vkticketscommon/common";
+
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+app.set("trust proxy", true); // trust ingress-nginx proxy
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // Will not encrypt the cookie
+    secure: process.env.NODE_ENV !== "test", // cookie will only be used if the user is usign https connection
+    // When jest run test it sets the process.env.NODE_ENV to "test"
+  }),
+);
+
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+/** handling not defined routes */
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
