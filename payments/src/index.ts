a5093424fd12ca0c6089953cb6bd53847d19c5ee
Vaibhav Kadwey <vaibhav3kadwey@gmail.com>
2026-08-09 16:16:41 +0530

    added : code for payments service setup

---
+++ payments/src/index.ts
@@ -0,0 +1,85 @@
+// import express from "express";
+// import "express-async-errors";
+// import { json } from "body-parser";
+import mongoose from "mongoose";
+// import cookieSession from "cookie-session";
+
+// import { currentUserRouter } from "./routes/current-user";
+// import { signUpRouter } from "./routes/signup";
+// import { signInRouter } from "./routes/signin";
+// import { signOutRouter } from "./routes/signout";
+// import { errorHandler } from "./middlewares/error-handler";
+// import { NotFoundError } from "./errors/not-found-error";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+// const app = express();
+// app.set("trust proxy", true); // trust ingress-nginx proxy
+// app.use(json());
+// app.use(
+//   cookieSession({
+//     signed: false, // Will not encrypt the cookie
+//     secure: true, // cookie will only be used if the user is usign https connection
+//   }),
+// );
+
+// app.use(currentUserRouter);
+// app.use(signUpRouter);
+// app.use(signInRouter);
+// app.use(signOutRouter);
+
+// /** handling not defined routes */
+// app.all("*", async () => {
+//   throw new NotFoundError();
+// });
+
+// app.use(errorHandler);
+
+const startApp = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT Not defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI Not defined");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID Not defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL Not defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID Not defined");
+  }
+  /** First we try to connect to database and when we have successfully connected */
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+
+    /** Gracefully closing the connection */
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+    /** Gracefully closing the connection */
+
+    /** Listening for upcoming events */
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to Mongo server");
+  } catch (err) {
+    console.error(err);
+  }
+
+  /** Start the server */
+  app.listen(3000, () => {
+    console.log("Listening on port: 3000 ");
+  });
+};
+
+startApp();
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,97 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+
+import { app } from "../app";
+import jwt from "jsonwebtoken";
+
+/** To tell typescript that a global property named getAuthCookie is present */
+
+declare global {
+  //   var getAuthCookie: () => Promise<string[]>;
+  var getAuthCookie: () => string[];
+}
+
+/** This file contains some hooks that facilitates writing our test much easier */
+
+jest.mock("../nats-wrapper");
+
+let mongo: any;
+/** Function runs before all our tests start's executing */
+beforeAll(async () => {
+  /** Setting environment variable for jwt */
+  process.env.JWT_KEY = "qwerty";
+  mongo = await MongoMemoryServer.create();
+
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+/** Function runs before each of our test */
+beforeEach(async () => {
+  jest.clearAllMocks();
+  if (mongoose.connection.db) {
+    const collections = await mongoose.connection.db?.collections();
+
+    for (let collection of collections) {
+      /** This will delte  */
+      await collection.deleteMany({});
+    }
+  }
+});
+
+/** Hook that runs after all our tests are complete */
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+/** Fucntion for making auth requests : Start :*/
+// global.getAuthCookie = async () => {
+global.getAuthCookie = () => {
+  //   const email = "test@test.com";
+  //   const password = "password";
+
+  //   const response = await request(app)
+  //     .post("/api/users/signup")
+  //     .send({
+  //       email,
+  //       password,
+  //     })
+  //     .expect(201);
+
+  //   const cookie = response.get("Set-Cookie");
+
+  //   if (!cookie) {
+  //     throw new Error("Failed to get cookie from response");
+  //   }
+
+  //   return cookie;
+
+  // Build a json web token payload. (id and email)
+
+  let paylod = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+
+  // Create the JWT
+  const token = jwt.sign(paylod, process.env.JWT_KEY!);
+
+  // Build session object
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
+
+/** Fucntion for making auth requests : End :*/
