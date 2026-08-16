3462d44ad1eaaa9938373c58b1b366c05c04432d
Vaibhav Kadwey <vaibhav3kadwey@gmail.com>
2026-08-16 20:02:32 +0530

    fixed : test cases issues

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -16,12 +16,10 @@ it("Clears the cookie after signout", async () => {
     .expect(200);
 
   const cookie = response.get("Set-Cookie");
-  if (!cookie) {
-    throw new Error("Expected cookie but got undefined.");
-  }
+  expect(cookie).toBeDefined();
 
   //   console.log(response.get("Set-Cookie"));
-  expect(cookie[0]).toEqual(
+  expect(cookie![0]).toEqual(
     "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly",
   );
 });
